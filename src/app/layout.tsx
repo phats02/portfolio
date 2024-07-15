61c4d11cd6d7df31d4bb016fbfe813f65d319d64
singhalutkarsh26 <singhalutkarsh26@gmail.com>
2024-07-15 13:28:13 +0000

    [ADDED : Circles]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,11 +27,20 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${sen.variable} ${josefinSans.variable}`}>
       <body
-        className={`${sen.className} w-screen min-h-screen m-0 p-0 overflow-x-hidden flex flex-col items-center`}
+        className={`${sen.className} w-screen min-h-screen relative m-0 p-0 overflow-x-hidden flex flex-col items-center`}
       >
-        {/* <main className="relative mx-auto mb-16 max-w-4xl px-8"> */}
         {children}
-        {/* </main> */}
+        <div className="absolute top-0 left-0 right-0">
+          <div className="absolute left-8 top-16 -z-10 overflow-visible opacity-20">
+            <div className="circle-1 -z-10 h-[400px] w-[700px] rounded-full bg-red-700 mix-blend-multiply blur-[128px]" />
+          </div>
+          <div className="absolute left-10 top-32 -z-10 overflow-visible opacity-20">
+            <div className="circle-2 -z-10 h-[400px] w-[700px] rounded-full bg-green-700 mix-blend-multiply blur-[128px]" />
+          </div>
+          <div className="absolute left-12 top-48 -z-10 overflow-visible opacity-20">
+            <div className="circle-3 -z-10 h-[600px] w-[500px] rounded-full bg-blue-700 mix-blend-multiply blur-[128px]" />
+          </div>
+        </div>
       </body>
     </html>
   );
