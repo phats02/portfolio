fc9062ff696ba1b72aac656cd80f4a880b74593d
Utkarsh Singhal <118098216+Utkarsh-Singhal-26@users.noreply.github.com>
2024-08-21 12:41:40 +0000

    [UPDATED : Experience]

---
+++ src/assets/data/experience.tsx
@@ -0,0 +1,21 @@
+interface ExperienceInterface {
+  id: number;
+  title: string;
+  company: string;
+  time: string;
+}
+
+export const experience: ExperienceInterface[] = [
+  {
+    id: 1,
+    title: "Product Engineer Intern",
+    company: "Requestly",
+    time: "08 / 2024 - Present",
+  },
+  {
+    id: 2,
+    title: "Full Stack Intern",
+    company: "Website Development and Maintenance Cell, VIPS-TC",
+    time: "04 / 2024 - Present",
+  },
+];
--- src/components/home/experience.tsx
+++ src/components/home/experience.tsx
@@ -1,17 +1,22 @@
+import { experience } from "@/assets/data/experience";
+
 const Experience = (): JSX.Element => {
   return (
     <div className="w-full flex flex-col gap-4">
       <h2 className="text-3xl font-bold font-josefin">Experience</h2>
-      <div className="grid grid-cols-3 rounded-xl border-[1px] border-tertiary bg-secondary/50 p-4 transition duration-200 hover:border-accent md:hover:scale-[1.01]">
-        <div className="col-span-2 flex flex-col gap-2">
-          <h3 className="text-lg font-semibold">
-            Full Stack Intern
-          </h3>
-          <p className="text-md text-gray-300 italic">
-            Website Development and Maintenance Cell, VIPS-TC
-          </p>
-        </div>
-        <p className="text-md text-gray-300 text-right">04 / 2024 - Present</p>
+      <div className="flex flex-col gap-4">
+        {experience.map((item) => (
+          <div
+            key={item.id}
+            className="grid grid-cols-3 rounded-xl border-[1px] border-tertiary bg-secondary/50 p-4 transition duration-200 hover:border-accent md:hover:scale-[1.01]"
+          >
+            <div className="col-span-2 flex flex-col">
+              <h3 className="text-lg font-semibold">{item.title}</h3>
+              <p className="text-md text-gray-300 italic">{item.company}</p>
+            </div>
+            <p className="text-md text-gray-300 text-right">{item.time}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
