185fb565eb09015f68387382c908dfbb9ba6a7e9
Utkarsh-Singhal-26 <singhalutkarsh26@gmail.com>
2025-02-04 19:25:59 +0530

    [ SETUP ] : Initial setup for v2 portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,7 @@
-import Home from "@/components/home";
-import Contact from "@/components/home/contact";
-import Experience from "@/components/home/experience";
-import Projects from "@/components/home/projects";
-import Skills from "@/components/home/skills";
-
 export default function Page() {
   return (
-    <div className="w-[90%] lg:w-[70%] xl:w-[50%] my-8 flex flex-col items-center gap-16">
-      <Home />
-      <Experience />
-      <Skills />
-      <Projects />
-      <Contact />
+    <div>
+      <h1>Page</h1>
     </div>
   );
 }
