f1726b313bb1514b3382b43a7761d4e9643061a1
Utkarsh-Singhal-26 <singhalutkarsh26@gmail.com>
2025-02-08 11:31:45 +0530

    [ ADDED ] : Configure security headers in next.config.ts; [ UPDATED ] : Update favicon.ico

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          }
+        ]
+      }
+    ]
+  },
 };
 
 export default nextConfig;
